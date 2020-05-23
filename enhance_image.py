640fd9c134a985e12ccc82e43c368753c78e5548
Joseph Afriyie Attakorah <juxluvjoe@gmail.com>
2020-05-23 17:44:55 +0300

    fixed get_roi

--- enhance_image.py
+++ enhance_image.py
@@ -1,18 +1,20 @@
 import numpy as np
 import cv2
-from utilities import *
 
 
 # This function applies all elaboration steps to the image
-def get_roi(raw_image):
-    height, width = raw_image.shape
+def get_roi(img):
 
+    new_frame_grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    height, width = new_frame_grey.shape
+
+    # points = np.array([bottom_left, top_left, top_center, top_right, bottom_right], np.int32)
     points = np.array([[150, 350], [200, 275.0], [320.0, 275.0], [410, 275.0], [460, 350]], np.int32)
     points = points.reshape((-1, 1, 2))
     black_image = np.zeros((height, width, 1), np.uint8)
 
     polygonal_shape = cv2.fillPoly(black_image, [points], (255, 255, 255))
-    colored_masked_road = cv2.bitwise_and(raw_image, raw_image, mask=polygonal_shape)
+    colored_masked_road = cv2.bitwise_and(new_frame_grey, new_frame_grey, mask=polygonal_shape)
 
     # convert back to BRG/jaa
     result = cv2.cvtColor(colored_masked_road, cv2.COLOR_GRAY2BGR)
